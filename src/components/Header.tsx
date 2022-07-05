f9d8cf9f0d0c186322c73af752fa0f6cc694e834
waiwai2525 <waiwai.cps@gmail.com>
2022-07-05 17:33:15 +0900

    feat: ロゴのサイズを調整しました

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" fixed="top" bg="dark" variant="dark">
+    <Navbar expand="lg" sticky="top" bg="dark" variant="dark">
       <Container fluid>
         <Navbar.Brand href="/" as={Link}>
           <a>
             <img
               src="/images/logo.svg"
-              height="auto"
-              width="250"
+              height="46"
+              width="auto"
               alt="Tokai Student Rocket Project"
             />
           </a>
@@ -18,17 +18,32 @@ const Header = () => {
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="ps-md-5">
-            <Link href="/" passHref>
-              <Nav.Link>Home</Nav.Link>
-            </Link>
             <Link href="/about" passHref>
               <Nav.Link>About</Nav.Link>
             </Link>
           </Nav>
           <Nav className="ms-auto pe-md-5">
-            <Nav.Link href="https://twitter.com/Tokai_SRP" target="_brank" rel="noreferrer noopener">Twitter</Nav.Link>
-            <Nav.Link href="https://www.instagram.com/tokai_srp/" target="_brank" rel="noreferrer noopener">Instagram</Nav.Link>
-            <Nav.Link href="https://www.youtube.com/c/TokaiSRP" target="_brank" rel="noreferrer noopener">Youtube</Nav.Link>
+            <Nav.Link
+              href="https://twitter.com/Tokai_SRP"
+              target="_brank"
+              rel="noreferrer noopener"
+            >
+              Twitter
+            </Nav.Link>
+            <Nav.Link
+              href="https://www.instagram.com/tokai_srp/"
+              target="_brank"
+              rel="noreferrer noopener"
+            >
+              Instagram
+            </Nav.Link>
+            <Nav.Link
+              href="https://www.youtube.com/c/TokaiSRP"
+              target="_brank"
+              rel="noreferrer noopener"
+            >
+              Youtube
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
