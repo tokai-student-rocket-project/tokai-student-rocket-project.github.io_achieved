5d7ce9e16ce474d629db581c9846b17ef9121b98
waiwai2525 <waiwai.cps@gmail.com>
2022-07-07 21:02:49 +0900

    feat: indexページを追加しました

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" sticky="top" bg="dark" variant="dark">
+    <Navbar expand="lg" fixed="top" bg="dark" variant="dark">
       <Container fluid>
         <Navbar.Brand href="/" as={Link}>
           <a>
@@ -19,7 +19,22 @@ const Header = () => {
         <Navbar.Collapse>
           <Nav className="ps-md-5">
             <Link href="/about" passHref>
-              <Nav.Link>About</Nav.Link>
+              <Nav.Link>ABOUT</Nav.Link>
+            </Link>
+            <Link href="/rocket" passHref>
+              <Nav.Link>ROCKET</Nav.Link>
+            </Link>
+            <Link href="/project" passHref>
+              <Nav.Link>PROJECT</Nav.Link>
+            </Link>
+            <Link href="/news" passHref>
+              <Nav.Link>NEWS</Nav.Link>
+            </Link>
+            <Link href="/contact" passHref>
+              <Nav.Link>CONTACT</Nav.Link>
+            </Link>
+            <Link href="/link" passHref>
+              <Nav.Link>LINK</Nav.Link>
             </Link>
           </Nav>
           <Nav className="ms-auto pe-md-5">
