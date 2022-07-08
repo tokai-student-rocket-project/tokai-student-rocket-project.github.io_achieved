d63ba00c5b965f8f566bb622bfbb9dd0a05247dd
waiwai2525 <waiwai.cps@gmail.com>
2022-07-08 21:18:52 +0900

    fix: ロゴをクリックした時にNav.Linkの選択を外すように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,37 @@
 import Link from "next/link";
+import { createRef, RefObject, useRef } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 import styles from "../styles/Header.module.scss";
 
+const links = [
+  { href: "/about", text: "ABOUT" },
+  { href: "/vehicle", text: "VEHICLE" },
+  { href: "/mission", text: "MISSION" },
+  { href: "/news", text: "NEWS" },
+  { href: "/contact", text: "CONTACT" },
+  { href: "/link", text: "LINK" },
+];
+
 const Header = () => {
+  const linkRefs = useRef<RefObject<HTMLAnchorElement>[]>([]);
+
+  const handleClickLogo = () => {
+    console.log("onClick");
+    linkRefs.current.map((linkRef) => {
+      linkRef.current?.classList.remove("active");
+    });
+  };
+
+  links.map((_, index) => {
+    linkRefs.current[index] = createRef();
+  });
+
   return (
     <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
       <Container fluid>
         <Navbar.Brand href="/" as={Link}>
-          <a>
+          <a onClick={handleClickLogo}>
             <img
               src="/images/logo.svg"
               height="46px"
@@ -20,24 +43,13 @@ const Header = () => {
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="ps-md-5">
-            <Link href="/about" passHref>
-              <Nav.Link>ABOUT</Nav.Link>
-            </Link>
-            <Link href="/vehicle" passHref>
-              <Nav.Link>VEHICLE</Nav.Link>
-            </Link>
-            <Link href="/mission" passHref>
-              <Nav.Link>MISSION</Nav.Link>
-            </Link>
-            <Link href="/news" passHref>
-              <Nav.Link>NEWS</Nav.Link>
-            </Link>
-            <Link href="/contact" passHref>
-              <Nav.Link>CONTACT</Nav.Link>
-            </Link>
-            <Link href="/link" passHref>
-              <Nav.Link>LINK</Nav.Link>
-            </Link>
+            {links.map((item, index) => {
+              return (
+                <Link key={index} href={item.href} passHref>
+                  <Nav.Link ref={linkRefs.current[index]}>{item.text}</Nav.Link>
+                </Link>
+              );
+            })}
           </Nav>
           <Navbar.Text className="ms-auto pe-md-5">
             <a
