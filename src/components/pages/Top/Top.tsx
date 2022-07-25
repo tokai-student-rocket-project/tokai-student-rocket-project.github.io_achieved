d5488e259bcb6cfb062f329407d2c022152c9c31
waiwai2525 <waiwai.cps@gmail.com>
2022-07-26 07:35:34 +0900

    feat: 画像を追加

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Img from "react-optimized-image";
 import styles from "./Top.module.scss";
 
@@ -14,27 +14,28 @@ export const Top = () => {
         />
       </div>
 
-      <Container style={{ padding: "64px 0" }}>
-        <Row>
-          <Col lg={6}>
-            <div style={{ color: "white" }}>
-              <p style={{ marginBottom: "32px" }}>
-                TSRPは手作りで低価格なロケット開発の場を学生に提供することで、学生が机上の勉学では学べない、
-                宇宙理工学の知識・技術を修得させ、将来の宇宙技術者を養成することを目的として1995年に設立されました。
-                ロケットの設計・制作、搭載計器の動作試験、実験から得られたデータ解析など多岐に渡るロケットミッションの活動を通じて、
-                技術面の向上だけではなく、マネジメント力、メンバーとの協調性、コミュニケーション能力など、学生の社会性の育成も目的としています。
-              </p>
-            </div>
-          </Col>
-          <Col lg={6}>
-            <img
-              src="https://fakeimg.pl/1920x1080/?text=TSRP&font=noto"
-              alt="tsrp"
-              style={{ height: "auto", width: "100%", objectFit: "cover" }}
-            />
-          </Col>
-        </Row>
+      <Container
+        style={{ color: "white", paddingTop: "64px", paddingBottom: "64px" }}
+      >
+        <p>
+          TSRPは手作りで低価格なロケット開発の場を学生に提供することで、学生が机上の勉学では学べない、
+          宇宙理工学の知識・技術を修得させ、将来の宇宙技術者を養成することを目的として1995年に設立されました。
+          ロケットの設計・制作、搭載計器の動作試験、実験から得られたデータ解析など多岐に渡るロケットミッションの活動を通じて、
+          技術面の向上だけではなく、マネジメント力、メンバーとの協調性、コミュニケーション能力など、学生の社会性の育成も目的としています。
+        </p>
       </Container>
+
+      <div className={styles["images"]}>
+        <div>
+          <Img src={require("./images/image0.jpg")} alt="image0" webp />
+        </div>
+        <div>
+          <Img src={require("./images/image1.jpg")} alt="image1" webp />
+        </div>
+        <div>
+          <Img src={require("./images/image2.jpg")} alt="image2" webp />
+        </div>
+      </div>
 
       <div className={styles["hero"]}>
         <img
@@ -59,11 +60,7 @@ export const Top = () => {
       </div>
 
       <div className={styles["hero"]}>
-        <img
-          src="https://fakeimg.pl/1920x1080/?text=Combustion&font=noto"
-          alt="combustion"
-          style={{ objectPosition: "60% 0" }}
-        />
+        <Img src={require("./images/combustion.jpg")} alt="combustion" webp />
         <div className={styles["hero-container"]}>
           <div>
             <div className={styles["headline-english"]}>Combustion</div>
