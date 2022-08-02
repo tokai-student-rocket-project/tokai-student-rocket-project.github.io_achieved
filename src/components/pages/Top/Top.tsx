c95aff327068f43f437cb777dd2dd38b6d09fc67
waiwai2525 <waiwai.cps@gmail.com>
2022-08-02 11:26:41 +0900

    refactor: できるだけbootstrapを使うように変更

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,6 +1,6 @@
 import ExportedImage from "next-image-export-optimizer";
 import { useEffect, useRef } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Stack } from "react-bootstrap";
 import styles from "./Top.module.scss";
 
 export const Top = () => {
@@ -25,10 +25,7 @@ export const Top = () => {
 
   return (
     <>
-      <div
-        className={styles["background-image"]}
-        ref={backgroundImageReference}
-      >
+      <div className="position-relative vh-100" ref={backgroundImageReference}>
         <ExportedImage
           src="images/pages/Top/tsrp.jpeg"
           layout="fill"
@@ -38,8 +35,8 @@ export const Top = () => {
         />
       </div>
 
-      <div className={styles["content-container"]}>
-        <Container style={{ paddingTop: "64px", paddingBottom: "48px" }}>
+      <Stack className="position-relative text-bg-dark">
+        <Container>
           <p>
             TSRPは手作りで低価格なロケット開発の場を学生に提供することで、学生が机上の勉学では学べない、
             宇宙理工学の知識・技術を修得させ、将来の宇宙技術者を養成することを目的として1995年に設立されました。
@@ -47,27 +44,27 @@ export const Top = () => {
             技術面の向上だけではなく、マネジメント力、メンバーとの協調性、コミュニケーション能力など、学生の社会性の育成も目的としています。
           </p>
         </Container>
+      </Stack>
 
-        <div className={styles["images"]}>
-          <ExportedImage
-            src="images/pages/Top/image0.jpg"
-            alt="image0"
-            layout="fill"
-            objectFit="cover"
-          />
-          <ExportedImage
-            src="images/pages/Top/image1.jpg"
-            alt="image1"
-            layout="fill"
-            objectFit="cover"
-          />
-          <ExportedImage
-            src="images/pages/Top/image2.jpg"
-            alt="image2"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
+      <div className={styles["gallery"]}>
+        <ExportedImage
+          src="images/pages/Top/image0.jpg"
+          alt="image0"
+          layout="fill"
+          objectFit="cover"
+        />
+        <ExportedImage
+          src="images/pages/Top/image1.jpg"
+          alt="image1"
+          layout="fill"
+          objectFit="cover"
+        />
+        <ExportedImage
+          src="images/pages/Top/image2.jpg"
+          alt="image2"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
     </>
   );
