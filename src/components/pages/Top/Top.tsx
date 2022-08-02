8f6733827c70d85e605baf186310564b762d2723
waiwai2525 <waiwai.cps@gmail.com>
2022-08-02 10:47:12 +0900

    feat: Topページからヒーローを削除

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,31 +1,10 @@
 import ExportedImage from "next-image-export-optimizer";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { useEffect, useRef } from "react";
+import { Container } from "react-bootstrap";
 import styles from "./Top.module.scss";
-
-const setShow = (
-  position: number,
-  element: HTMLDivElement | null,
-  setFunction: Dispatch<SetStateAction<boolean>>
-) => {
-  if (element == null) {
-    return;
-  }
-
-  setFunction(position > element.offsetTop + element.clientHeight / 2);
-};
 
 export const Top = () => {
   const tsrpRef = useRef<HTMLDivElement>(null);
-
-  const mechanismRef = useRef<HTMLDivElement>(null);
-  const [showMechanism, setShowMechanism] = useState(false);
-
-  const combustionRef = useRef<HTMLDivElement>(null);
-  const [showCombustion, setShowCombustion] = useState(false);
-
-  const electricRef = useRef<HTMLDivElement>(null);
-  const [showElectric, setShowElectric] = useState(false);
 
   const handleScroll = () => {
     const position = window.scrollY;
@@ -33,10 +12,6 @@ export const Top = () => {
     if (tsrpRef.current != null) {
       tsrpRef.current.style.top = `${position - position / 2}px`;
     }
-
-    setShow(position, mechanismRef.current, setShowMechanism);
-    setShow(position, combustionRef.current, setShowCombustion);
-    setShow(position, electricRef.current, setShowElectric);
   };
 
   useEffect(() => {
@@ -47,7 +22,7 @@ export const Top = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <div className={styles["hero"]} ref={tsrpRef}>
         <ExportedImage
           src="images/pages/Top/tsrp.jpeg"
@@ -88,91 +63,7 @@ export const Top = () => {
             objectFit="cover"
           />
         </div>
-
-        <div className={styles["hero"]} ref={mechanismRef}>
-          <ExportedImage
-            src="https://fakeimg.pl/1920x1080/?text=Mechanism&font=noto"
-            alt="mechanism"
-            layout="fill"
-            objectFit="cover"
-          />
-          <div
-            className={`${styles["hero-container"]} ${
-              showMechanism ? styles["fade-in"] : ""
-            }`}
-          >
-            <div>
-              <div className={styles["headline-english"]}>Mechanism</div>
-              <div className={styles["headline-japanese"]}>＿＿ 構造機構班</div>
-              <p>
-                ロケットのボディなどの設計と製造を担当しています。
-                ロケットは上昇後、上空でパラシュートを開傘して地上へ軟着陸しますが、そのためのパラシュート放出機構も開発しています。
-                ロケットに関する全ての設計は最終的に構造機構班のチーフデザイナーへ集約されます。
-              </p>
-              <Button size="lg" variant="outline-light">
-                詳細を見る
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles["hero"]} ref={combustionRef}>
-          <ExportedImage
-            src="images/pages/Top/combustion.jpg"
-            alt="combustion"
-            layout="fill"
-            objectFit="cover"
-          />
-          <div
-            className={`${styles["hero-container"]} ${
-              showCombustion ? styles["fade-in"] : ""
-            }`}
-          >
-            <div>
-              <div className={styles["headline-english"]}>Combustion</div>
-              <div className={styles["headline-japanese"]}>＿＿ 燃焼班</div>
-              <p>
-                ハイブリッドロケットと呼ばれるロケットエンジンの開発を担当しています。
-                エンジンの設計や固体燃料の研究を行い、より大型で高性能なエンジンの実現を追求しています。
-                大学構内でロケットエンジンの動作テスト（燃焼実験）も行っています。
-                また、ロケット発射台の保守と運用も行っています。
-                ロケット発射台はこれらの活動はTSRPの打上げのみならず他大学の打上げでも必要とされるため、他大学の打上げ作業でも大きく活躍しています。
-              </p>
-              <Button size="lg" variant="outline-light">
-                詳細を見る
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles["hero"]} ref={electricRef}>
-          <ExportedImage
-            src="https://fakeimg.pl/1920x1080/?text=Electric&font=noto"
-            alt="electric"
-            layout="fill"
-            objectFit="cover"
-          />
-          <div
-            className={`${styles["hero-container"]} ${
-              showElectric ? styles["fade-in"] : ""
-            }`}
-          >
-            <div>
-              <div className={styles["headline-english"]}>Electric</div>
-              <div className={styles["headline-japanese"]}>＿＿ 計測制御班</div>
-              <p>
-                ロケットに搭載される電子機器の開発を担当します。
-                ロケットには必ず電子機器が搭載されます。
-                これはロケットの姿勢や位置、パラシュートの開傘指令などを出すために必要だからです。
-                打上実験後にはロケット搭載計器のデータが読み出され、データは全ての班での開発に役立てられます。
-              </p>
-              <Button size="lg" variant="outline-light">
-                詳細を見る
-              </Button>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
