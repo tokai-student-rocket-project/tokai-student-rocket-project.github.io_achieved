4095625383d224bfb0281e989f87bfaa3e93c161
Hiroki <hiroki1815@outlook.jp>
2023-03-14 00:17:13 +0900

    Siteのタイトルを変更

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Tokai Student Rocket Project";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "東海大学学生ロケットプロジェクトのホームページへようこそ！";
+export const TWITTER_HANDLE = "@Tokai_SRP";
+export const MY_NAME = "Tokai Student Rocket Project";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
